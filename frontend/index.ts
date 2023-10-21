5244dd84fe5bc189a308fb5d4b2e9aaa7c3c0154
DylanSmith63 <dylanmsmith.63@gmail.com>
2023-10-21 17:42:26 -0500

    form page

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,30 +1,37 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const handlebars = require("express-handlebars");
 
 const PORT = 3000;
 
 const app = express();
-app.set('view engine', 'handlebars');
-app.engine('handlebars', handlebars.engine({
-  layoutsDir: __dirname + '/views/layouts',
-}));
-app.use(express.static('public'));
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('landing', {layout : 'formLayout', css: 'landing.css'});
+app.get("/", (req, res) => {
+  res.render("landing", { layout: "formLayout", css: "landing.css" });
 });
 
-app.get('/score', (req, res) => {
-  if(
+app.get("/form", (req, res) => {
+  res.render("form", { layout: "formLayout", css: "form.css" });
+});
+
+app.get("/score", (req, res) => {
+  if (
     Object.keys(req.query).length !== 1 ||
-    !('score' in req.query) ||
+    !("score" in req.query) ||
     !/^((\d+(\.\d+)?)|(\.\d+))$/.test(req.query.score)
   ) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
   console.log(req.query);
-  res.render('score', {layout : 'formLayout', score: req.query.score});
+  res.render("score", { layout: "formLayout", score: req.query.score });
 });
 
 app.listen(PORT, () => {
